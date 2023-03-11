af749e2279290e91de9ba8f1db1119637a22b7de
ruslan-kornich <korny4rus143@gmail.com>
2023-03-11 11:11:39 +0200

    update

--- AdressBook.py
+++ AdressBook.py
@@ -1,12 +1,26 @@
+import re
 from collections import UserDict
+from datetime import datetime
 
 
 class Field:
     def __init__(self, value) -> None:
+        self._value = None
         self.value = value
 
     def __str__(self):
-        return f"{self.value}"
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self.value)
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
 
 
 class Name(Field):
@@ -14,21 +28,67 @@ class Name(Field):
 
 
 class Phone(Field):
-    pass
+
+    @staticmethod
+    def verify_phone(value):
+        phone = re.findall(r'\+\d{12}|0\d{9}', value)
+        if not phone:
+            raise ValueError
+
+    @Field.value.setter
+    def value(self, value):
+        self.verify_phone(value)
+        self._value = value
+
+
+class Birthday(Field):
+
+    @staticmethod
+    def verify_birthday(value):
+        birthday = re.findall(r'\d{1,2}\.\d{1,2}\.\d{4}', value)
+        if not birthday:
+            raise ValueError
+        return birthday
+
+    @Field.value.setter
+    def value(self, value):
+        self.verify_birthday(value)
+        self._value = datetime.strptime(value, "%d.%m.%Y").date()
 
 
 class Record:
-    def __init__(self, name: Name, phone=None) -> None:
+    def __init__(self, name: Name, phone=None, birthday=None) -> None:
         self.name = Name(name)
         self.phones = []
+        self.birthday = None
+
         if phone:
             self.add_phone(phone)
 
-    def __repr__(self, phone):
-        return f"{self.name}: {self.phones}"
+        if birthday:
+            self.add_birthday(birthday)
 
-    def add_phone(self, phone):
-        return self.phones.append(phone)
+    def __repr__(self):
+        return f"{self.name}: {self.phones}, {self.birthday}"
+
+    def add_phone(self, new_phone):
+        if not self.phones:
+            self.phones.append(new_phone)
+            return f"Phone '{new_phone}' is added"
+        else:
+            for phone in self.phones:
+                if phone != new_phone:
+                    self.phones.append(new_phone)
+                    return f"Phone '{new_phone}' is added"
+                else:
+                    return f"Phone '{new_phone}' already exist in AddressBook. Try again!"
+
+    def add_birthday(self, birthday):
+        if self.birthday:
+            return f"The date of birthday already exist in contact '{self.name}'. Try again!"
+        else:
+            self.birthday = Birthday(birthday)
+            return f"Date of birthday is added to the contact '{self.name}'"
 
     def change_phone(self, old_phone, new_phone):
         for phone in self.phones:
@@ -39,13 +99,54 @@ class Record:
 
     def remove_phone(self, del_phone):
         for phone in self.phones:
-            if phone.value == del_phone:
+            if phone == del_phone:
                 self.phones.remove(phone)
                 return f"Phone '{del_phone}' is delete"
         return f"Phone '{del_phone}' is not in AddressBook. Try again!"
 
+    def change_birthday(self, new_birth):
+        self.birthday = new_birth
+        return f"Date of birthday '{self.name}' is changed: '{new_birth}'"
 
-class AddressBook(UserDict):
+    def remove_birthday(self):
+        if self.birthday:
+            self.birthday = None
+            return f"Date of birthday is deleted"
+        return f"This contact does not have a date of birth"
+
+    def days_to_birthday(self):
+        today = datetime.now().date()
+        birthday = self.birthday.value.replace(year=today.year)
+        if birthday > today:
+            delta = (birthday - today).days
+        else:
+            delta = (birthday.replace(birthday.year + 1) - today).days
+        return delta
+
+
+class AdressBook(UserDict):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.count = 0
+
+    def __str__(self):
+        return f"{self.data}"
+
+    def __repr__(self):
+        return f"{self.data}"
+
     def add_record(self, record):
-        self.data[record.name.value] = record
-        return self.data
+        if record.name.value not in self.data:
+            self.data[record.name.value] = record
+            return f"New contact '{record.name.value}' is added"
+        else:
+            return f"This contact already exist in AddressBook"
+
+    def iterator(self, n):
+        names_in_page = list(self.data.keys())
+        if self.count >= len(names_in_page):
+            raise StopIteration
+        result_list = names_in_page[self.count: min(self.count + n, len(names_in_page))]
+        for i in result_list:
+            self.count += 1
+        yield result_list
--- cli_assistant.py
+++ cli_assistant.py
@@ -1,6 +1,6 @@
 from time import sleep
 
-from AdressBook import AddressBook, Record
+from AdressBook import AdressBook, Record
 
 
 def input_error(func):
@@ -11,10 +11,13 @@ def input_error(func):
             msg = "Contact is not found. Try again!"
             return msg
         except ValueError:
-            msg = "You didn't enter the phone number. Try again!"
+            msg = "Invalid format of data. Try again!"
             return msg
         except IndexError:
             msg = "You didn't enter the contact name or phone. Try again!"
+            return msg
+        except RuntimeError:
+            msg = "This is the end of AddressBook"
             return msg
 
     return wrapper
@@ -41,15 +44,16 @@ def main():
 
 @input_error
 def handle_user_input(user_input: str) -> tuple:
-    """The function decodes the command and contact information"""
+    """The function recognize the command and information about the contact"""
 
-    data = user_input.split()
-    command, user_data = data[0], data[1:]
+    command, user_data = user_input.split(" ")[0], user_input.split(" ")[1:]
     return command, user_data
 
 
 @input_error
 def hello_handler(*args, **kwargs) -> str:
+    """The function reads from the file and displays the welcome text on the screen"""
+
     with open('hello.txt', "r") as hello:
         for line in hello.readlines():
             print(line, end='')
@@ -58,37 +62,67 @@ def hello_handler(*args, **kwargs) -> str:
 
 @input_error
 def add_record(user_data: list) -> str:
-    """The function adds a new contact to the contact list"""
+    """Function to add a new contact to the list of contacts"""
 
-    name, phone = user_data[0], [] if not user_data[1:] else user_data[1:]
-    user_data = Record(name, phone)
-    BOOK.add_record(user_data)
-    return f"New contact '{name}' is added"
+    name = user_data[0]
+    if len(user_data) == 1:
+        phone = []
+        birthday = None
+    elif len(user_data) == 2:
+        phone = user_data[1]
+        birthday = None
+    else:
+        phone = user_data[1]
+        birthday = user_data[2]
+
+    return BOOK.add_record(Record(name, phone, birthday))
 
 
 @input_error
 def add_phone(user_data: list) -> str:
-    BOOK[user_data[0]].add_phone(user_data[1])
-    return f"Phone number is added to the contact '{user_data[0]}'"
+    """The function of adding a new phone number to the return contact"""
+
+    return BOOK[user_data[0]].add_phone(user_data[1])
 
 
 @input_error
-def change_handler(user_data: list) -> str:
-    """The function stores the new phone number of an existing contact"""
+def add_birthday(user_data: list) -> str:
+    """Function to add the date of birth to the contact"""
+
+    return BOOK[user_data[0]].add_birthday(user_data[1])
+
+
+@input_error
+def change_birthday(user_data: list) -> str:
+    """Function to change the date of birth of a contact"""
+
+    return BOOK[user_data[0]].change_birthday(user_data[1])
+
+
+@input_error
+def del_birthday(user_data: list) -> str:
+    """Function to see the date of birth of a contact"""
+
+    return BOOK[user_data[0]].remove_birthday()
+
+
+@input_error
+def change_phone(user_data: list) -> str:
+    """Функція замінює номер телефону існуючого контакту"""
 
     return BOOK[user_data[0]].change_phone(user_data[1], user_data[2])
 
 
 @input_error
 def show_phone_handler(user_data: list) -> str:
-    """The function displays the phone number of an existing contact"""
+    """Function to display on the screen the phone number of the current contact"""
 
     return "%s" % BOOK.get((user_data[0]), "Contact is not found. Try again!")
 
 
 @input_error
 def bye_handler(*args, **kwargs):
-    """End of dialogue"""
+    """The end of the dialogue"""
 
     print("Goodbye!")
     sleep(3)
@@ -97,7 +131,7 @@ def bye_handler(*args, **kwargs):
 
 @input_error
 def del_record(user_data: list) -> str:
-    """The function removes a contact from the contact list"""
+    """The function to view a contact from the list of contacts or the phone number of a contact"""
 
     del BOOK[user_data[0]]
     return f"'{user_data[0]}' is delete"
@@ -105,34 +139,57 @@ def del_record(user_data: list) -> str:
 
 @input_error
 def del_phone(user_data: list) -> str:
-    """The function removes a phones from the contact list"""
+    """"The function of viewing the indications of the telephone number of the contact"""
 
     return BOOK[user_data[0]].remove_phone(user_data[1])
 
 
 @input_error
 def show_all_handler(*args, **kwargs) -> str:
-    """The function displays the entire list of contacts"""
+    """The function to display the entire list of contacts on the screen"""
 
     dict_to_str, count = [], 0
     for key, value in BOOK.items():
         count += 1
-        dict_to_str.append("  {}. {}".format(count, value))
+        if value.birthday:
+            days = BOOK[key].days_to_birthday()
+            dict_to_str.append(f"  {count}. {value}, {days} days to birthday")
+        else:
+            dict_to_str.append(f"  {count}. {value}")
     dict_output = "\n".join(dict_to_str)
 
     return f"{dict_output}"
 
 
-BOOK = AddressBook()
+@input_error
+def pagination(user_data: list):
+    num = int(user_data[0])
+    pages = BOOK.iterator(num)
+    result = ''
+    for page in pages:
+        for name in page:
+            result += f"\n {BOOK[name]}"
+    return result
+
+
+BOOK = AdressBook()
+BOOK.add_record(Record("john"))
+BOOK.add_record(Record("alex", '+123456789101', '10.10.1995'))
+BOOK.add_record(Record("harry", '+123456789102'))
+BOOK.add_record(Record("george", '+123456789102'))
 
 HANDLER = {
     "hello": hello_handler,
     "add": add_record,
     "add_phone": add_phone,
-    "change": change_handler,
-    "show_all": show_all_handler,
-    "del": del_record,
+    "change_phone": change_phone,
     "del_phone": del_phone,
+    "add_birthday": add_birthday,
+    "change_birthday": change_birthday,
+    "del_birthday": del_birthday,
+    "show_all": show_all_handler,
+    "show": pagination,
+    "del": del_record,
     "phone": show_phone_handler,
     "exit": bye_handler,
     "close": bye_handler,
